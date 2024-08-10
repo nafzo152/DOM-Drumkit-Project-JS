a3fa703795865339574f4172717488468a149b8e
Cargo <ahmednafisbd15@gmail.com>
2024-08-10 22:24:31 +0800

    added the array and array length

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,4 +1,9 @@
- document.querySelector("button").addEventListener("click", handleClick);
+ var numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+ for (var i=0; i<numberofDrumButtons; i++)
+ 
+ 
+ document.querySelector(".drum")[i].addEventListener("click", handleClick);
 
 function handleClick() {
     alert("I got clicked");
