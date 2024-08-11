95fb020df549878b6df1bb71d13a18a21cc81ee4
Cargo <ahmednafisbd15@gmail.com>
2024-08-11 12:30:00 +0800

    change all variable audios to specific drum kits

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,40 +6,42 @@ for (var i = 0; i < numberofDrumButtons; i++) {
         var buttonInnerHTML = this.innerHTML;
 
         switch (buttonInnerHTML) {
-            case "w";
-                var audio = new Audio("sounds/tom-1.mp3");
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
                 audio.play();
                 break;
 
-            case "a";
-                var audio = new Audio("sounds/tom-2.mp3");
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
                 audio.play();
                 break;
 
-            case "s";
-                var audio = new Audio("sounds/tom-3.mp3");
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
                 audio.play();
                 break;
 
-            case "d";
-                var audio = new Audio("sounds/tom-4.mp3");
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
                 audio.play();
                 break;
 
-            case "j";
-                var audio = new Audio("sounds/snare.mp3");
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
                 audio.play();
                 break;
 
-            case "k";
-                var audio = new Audio("sounds/crash.mp3");
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
                 audio.play();
                 break;
 
-            case "l";
-                var audio = new Audio("sounds/kick-bass.mp3");
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
                 audio.play();
                 break;
+
+            default:
         }
 
 
