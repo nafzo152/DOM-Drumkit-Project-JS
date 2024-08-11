579645fac79672d30f958bd87dab3f17f27b0da6
Cargo <ahmednafisbd15@gmail.com>
2024-08-11 12:12:55 +0800

    added audio function to the html

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,7 +2,8 @@ var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberofDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
     });
 
 }
