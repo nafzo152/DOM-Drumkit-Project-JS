c6570b83ab0690717919f8ab7257ce91265bafac
Cargo <ahmednafisbd15@gmail.com>
2024-08-11 12:21:59 +0800

    added switch statements

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,6 +2,54 @@ var numberofDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberofDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "a";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "s";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "d";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "j";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "k";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+
+            case "l";
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+        }
+
+
+
+
+
+
+
+
+
         var audio = new Audio("sounds/tom-1.mp3");
         audio.play();
     });
