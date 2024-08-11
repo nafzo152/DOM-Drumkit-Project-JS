ec0bdea3b670b64ef60426d22e5ff950d6ddf160
Cargo <ahmednafisbd15@gmail.com>
2024-08-11 12:23:39 +0800

    added sounds to the switch cases

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -12,32 +12,32 @@ for (var i = 0; i < numberofDrumButtons; i++) {
                 break;
 
             case "a";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/tom-2.mp3");
                 audio.play();
                 break;
 
             case "s";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/tom-3.mp3");
                 audio.play();
                 break;
 
             case "d";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/tom-4.mp3");
                 audio.play();
                 break;
 
             case "j";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/snare.mp3");
                 audio.play();
                 break;
 
             case "k";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/crash.mp3");
                 audio.play();
                 break;
 
             case "l";
-                var audio = new Audio("sounds/tom-1.mp3");
+                var audio = new Audio("sounds/kick-bass.mp3");
                 audio.play();
                 break;
         }
